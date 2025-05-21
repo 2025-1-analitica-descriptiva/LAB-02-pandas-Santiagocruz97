620cb1d1aae206bbcbc8dceec3da2d1a7cc14953
Santiago C. <scruza@unal.edu.co>
2025-05-21 05:05:10 -0500

    .

--- homework/pregunta_09.py
+++ homework/pregunta_09.py
@@ -4,10 +4,15 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_09():
-    """
+    df = pd.read_csv("files/input/tbl0.tsv", delimiter="\t")
+    df["c3"] = pd.to_datetime(df["c3"])      # Convertir a tipo datetime
+    df["year"] = df["c3"].dt.year.astype(str)
+    return df
+print(pregunta_09())
+"""
     Agregue el año como una columna al dataframe que contiene el archivo
     `tbl0.tsv`.
 
--- homework/pregunta_10.py
+++ homework/pregunta_10.py
@@ -6,8 +6,20 @@ librerias de pandas para resolver las preguntas.
 """
 
 
+import pandas as pd
+
 def pregunta_10():
-    """
+    df = pd.read_csv("files/input/tbl0.tsv", delimiter="\t")
+
+    tabla = (
+        df.groupby("c1")["c2"]
+        .apply(lambda x: ":".join(str(i) for i in sorted(x)))
+        .to_frame()
+    )
+
+    return tabla
+print(pregunta_10())
+"""
     Construya una tabla que contenga `c1` y una lista separada por ':' de los
     valores de la columna `c2` para el archivo `tbl0.tsv`.
 
